ca3d9a6a97f384b4ccb8a3b320972d1424fcd03a
kmartynski <k.martynski@ymail.com>
2019-03-18 23:58:08 +0100

    Next progress

--- js/app.js
+++ js/app.js
@@ -4,90 +4,92 @@ var tbody = $("#books");
 
 tbody.on("click", ".details", function(event) {
 
-    event.preventDefault();
+        event.preventDefault();
 
-    var link = $(this);
-    var tr = $(event.target).closest("tr");
+        var link = $(this);
+        var tr = $(event.target).closest("tr");
 
-    if (link.text() === "show") {
-        link.text("hide");
-        var bookId = link.data("id");
-        $.get(`http://localhost:8000/book/${bookId}`)
-            .done(function(book) {
+        if (link.text() === "show") {
+            link.text("hide");
+            var bookId = link.data("id");
+            $.get(`http://localhost:8000/book/${bookId}`)
+                .done(function(book) {
 
-                var GENRES = {
-                    1: "Romans",
-                    2: "Obyczajowa",
-                    3: "Sci-fi i fantasy",
-                    4: "Literatura faktu",
-                    5: "Popularnonaukowa",
-                    6: "Poradnik",
-                    7: "Kryminał, sensacja"
-                    };
+                    var GENRES = {
+                        1: "Romans",
+                        2: "Obyczajowa",
+                        3: "Sci-fi i fantasy",
+                        4: "Literatura faktu",
+                        5: "Popularnonaukowa",
+                        6: "Poradnik",
+                        7: "Kryminał, sensacja"
+                        };
 
-                var details = $('<tr class="bookDetails">');
+                    var details = $('<tr class="bookDetails">');
 
-                var p1 = $(`<p><b>ISBN</b>: ${book.isbn}</p>`);
-                var p2 = $(`<p><b>Publisher</b>: ${book.publisher}</p>`);
-                var p3 = $(`<p><b>Genre</b>: ${GENRES[book.genre]}</p>`);
-                var p4 = $(`<p><b>ID</b>: ${book.id}</p>`);
+                    var p1 = $(`<p><b>ISBN</b>: ${book.isbn}</p>`);
+                    var p2 = $(`<p><b>Publisher</b>: ${book.publisher}</p>`);
+                    var p3 = $(`<p><b>Genre</b>: ${GENRES[book.genre]}</p>`);
+                    var p4 = $(`<p><b>ID</b>: ${book.id}</p>`);
 
-                details.append(
-                    $('<td colspan="3">').append(p1)
-                                         .append(p2)
-                                         .append(p3)
-                                         .append(p4)
+                    details.append(
+                        $('<td colspan="3">').append(p1)
+                                             .append(p2)
+                                             .append(p3)
+                                             .append(p4)
 
-                );
+                    );
 
-                details.insertAfter(tr);
-            })
-            .fail(function() {
-                alert("Nie można połączyć się z serwerem");
-            });
-    } else {
-        link.text("show");
-        tr.next().remove();
-    }
-});
-
-$.get("http://localhost:8000/book/")
-    .done(function(books) {
-
-        tbody.empty();
-
-        for (var i = 0; i < books.length; i++) {
-
-            var tr = $("<tr>");
-            tr.append($(`<td>${books[i].author}</td>`));
-            tr.append($(`<td>${books[i].title}</td>`));
-            tr.append($(`<td><a class="details" href="#" data-id="${books[i].id}">show</a></td>`));
-            tr.append($(`<td><a href="#" class="delete">delete</a></td>`));
-
-            tbody.append(tr);
+                    details.insertAfter(tr);
+                })
+                .fail(function() {
+                    alert("Nie można połączyć się z serwerem");
+                });
+        } else {
+            link.text("show");
+            tr.next().remove();
         }
     });
 
-$("form").on("submit", function( event ) {
-    event.preventDefault();
-    var formData = $("form").serialize();
-    console.log(formData);
+    $.get("http://localhost:8000/book/")
+        .done(function(books) {
 
-    $.post("http://localhost:8000/book/", formData)
-        .done(function () {
-            console.log("Poszło")
-        })
-});
-$(".delete").click(function () {
-    $.ajax({
+            tbody.empty();
+
+            for (var i = 0; i < books.length; i++) {
+
+                var tr = $("<tr>");
+                tr.append($(`<td>${books[i].author}</td>`));
+                tr.append($(`<td>${books[i].title}</td>`));
+                tr.append($(`<td><a class="details" href="#" data-id="${books[i].id}">show</a></td>`));
+                tr.append($(`<td><a class="delete" href="#">delete</a></td>`));
+
+                tbody.append(tr);
+            }
+    }).always($(".delete").on("click", ".delete",function (event) {
+        event.preventDefault();
+        console.log("Działa");
+        $.ajax({
         url: `http://localhost:8000/book/${$(".delete").attr("id")}`,
         data: {},
-        type: "Delete",
+        type: "delete",
         dataType: "json"
+        });
+        console.log("Delete poszlo");
+        location.reload()
+    }));
+
+    $("form").on("submit", function( event ) {
+        event.preventDefault();
+        var formData = $("form").serialize();
+        console.log(formData);
+
+        $.post("http://localhost:8000/book/", formData)
+            .done(function () {
+                console.log("Poszło")
+            })
+
     });
-    console.log("Delete poszlo");
-    location.reload()
-});
 });
 
 
